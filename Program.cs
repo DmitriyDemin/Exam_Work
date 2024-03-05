c06bd09da809bad865c4aac36b29bc5f496f2f2d
Dmitriy_Demin <d.demin@inbox.ru>
2024-03-05 20:59:06 +0400

    add_conditions

--- Program.cs
+++ Program.cs
@@ -2,8 +2,6 @@
 namespace Exam_Work;
 class Work_string
 {
-    
-
     public static void Main()
     {
         Console.Clear();
@@ -12,10 +10,27 @@ class Work_string
         Console.WriteLine("Введите элементы строчного массива");
         for (int i= 0; i< set_strings.Length; i++)
         {
-            set_strings[i] = Console.ReadLine();
+            set_strings[i] = Console.ReadLine()!;
         }
-        Console.WriteLine($"Начальный массив: [{string.Join(", ", set_strings)}]");
-    
+        // for (int j= 0; j< set_strings.Length; j++)
+        // {
+        //     if (set_strings[j] == " " | set_strings[j] == "")
+        //     Console.WriteLine($"Начальный массив: [fgfh, 342, !2, %]");
+        //  }
+        Console.Write("Начальный массив: ");
+        for (int j= 0; j< set_strings.Length; j++)
+        {
+            Console.Write($"[{set_strings[j]}]");
+        }
+        Console.WriteLine();
+        string[] set_new = new string[4];
+        for (int k= 0; k< set_strings.Length; k++)
+        {
+            string a = set_strings[k]; 
+            if (a.Length <=3)
+            set_new[k] = set_strings[k];
+        }
+    Console.WriteLine($"массив новых строк: [{string.Join(", ", set_new)}]");
     }
 
 
