4823373da985397a135c082a1c79c5be217ea15c
Dmitriy_Demin <d.demin@inbox.ru>
2024-03-05 20:27:52 +0400

    create_arr_string

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+namespace Exam_Work;
+class Work_string
+{
+    
+
+    public static void Main()
+    {
+        Console.Clear();
+        string[] set_strings = new string [4];
+        
+        Console.WriteLine("Введите элементы строчного массива");
+        for (int i= 0; i< set_strings.Length; i++)
+        {
+            set_strings[i] = Console.ReadLine();
+        }
+        Console.WriteLine($"Начальный массив: [{string.Join(", ", set_strings)}]");
+    
+    }
+
+
+}
+
