07a089ff4df30bd84a0b1d4602efed180a1af5b6
Dmitriy_Demin <d.demin@inbox.ru>
2024-03-06 20:07:16 +0400

    double quotes

--- Program.cs
+++ Program.cs
@@ -1,27 +1,33 @@
-﻿using System;
+﻿/*
+Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+using System;
 namespace Exam_Work;
 class Work_string
 {
     public static void Main()
     {
         Console.Clear();
-        string[] set_strings = new string [4];
-        
+        Console.WriteLine("Введите количество элементов массива");
+        int size = int.Parse(Console.ReadLine()!);
+        string[] set_strings = new string [size];
         Console.WriteLine("Введите элементы строчного массива");
         for (int i= 0; i< set_strings.Length; i++)
         {
             set_strings[i] = Console.ReadLine()!;
         }
-        // for (int j= 0; j< set_strings.Length; j++)
-        // {
-        //     if (set_strings[j] == " " | set_strings[j] == "")
-        //     Console.WriteLine($"Начальный массив: [fgfh, 342, !2, %]");
-        //  }
-        Console.Write("Начальный массив: ");
-        for (int j= 0; j< set_strings.Length; j++)
-        {
-            Console.Write($"[{set_strings[j]}]");
-        }
+        Console.Write($"Начальный массив: [\"{string.Join("\" ,\"", set_strings)}\"]");
+        
+        // }
         Console.WriteLine();
         string[] set_new = new string[4];
         for (int k= 0; k< set_strings.Length; k++)
@@ -30,9 +36,11 @@ class Work_string
             if (a.Length <=3)
             set_new[k] = set_strings[k];
         }
-    Console.WriteLine($"массив новых строк: [{string.Join(", ", set_new)}]");
+        Console.WriteLine(string.Join(" ", set_new));
+    // Console.WriteLine($"массив новых строк: [{string.Join(", ", set_new)}]");
+    
+    
     }
 
 
 }
-
